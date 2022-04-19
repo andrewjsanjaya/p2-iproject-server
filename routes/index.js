343770bbec32c1657a1e71d1a99a169ecc41b8b6
andrewjsanjaya <andrew.j.sanjaya@gmail.com>
2022-04-20 00:33:30 +0700

    adding show playlist based on weather, also adding axios

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./routeUser");
+const Playlist = require("./routePlaylist");
 
 router.use("/users", User);
+router.use("/playlists", Playlist);
 
 module.exports = router;
